95438654f8137062a03679aba3398eeb3ea61f78
alex <alexwilliamgomessilva168@gmail.com>
2024-04-30 17:24:00 -0300

    processo de formulario no php

--- formulariobiblioteca.php
+++ formulariobiblioteca.php
@@ -2,6 +2,35 @@
     $nome = $_POST['nome'];
     $sobrenome = $_POST ['sobrenome'];
     $email = $_POST ['email'];
-    echo $nome;
+    $cpf = $_POST ['cpf'];
+    $dtnasc = $_POST ['dtnasc'];
+    $categoria = $_POST ['categoria'];
 
+    echo $nome;
+    echo '<p></p>';
+    echo $sobrenome;
+    echo '<p></p>';
+    echo $email;
+    echo '<p></p>';
+    echo $cpf;
+    echo '<p></p>';
+    echo $dtnasc;
+    echo '<p></p>';
+    echo $categoria;
+    echo '<p></p>';
+
+
+    $autor = $_POST['autor'];
+    $nomedolivro = $_POST ['nomedolivro'];
+    $numero_pag = $_POST ['numero_pag'];
+
+    echo $autor;
+    echo $nomedolivro;
+    echo $numero_pag;
+
+    $class = $_POST ['class'];
+    echo $class;
+
+    $comentarios = $_POST ['comentarios'];
+    echo $comentarios;
 ?>
